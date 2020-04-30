15a49a74372991e9ec38efe8bef9215e664bf8c6
Kemikals <waitinferage21@gmail.com>
2020-04-29 23:11:08 -1000

    small fixes to Code command

--- src/main/java/org/learn_java/commands/Code.java
+++ src/main/java/org/learn_java/commands/Code.java
@@ -2,15 +2,19 @@ package org.learn_java.commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
 
 public class Code extends Command {
 
     private Message message;
+    private EventWaiter waiter;
 
     public Code(){
         this.name = "code";
+        this.help = "Messages channel detailing how to format code in a code block";
+        this.cooldown = 5;
         message = buildMessage();
     }
 
@@ -22,6 +26,7 @@ public class Code extends Command {
         builder.append("\n\\`\\`\\`");
         builder.append("\n Which will result in a code block such as");
         builder.appendCodeBlock("int x = 3\nSystem.out.println(x);", "java");
+        builder.append("\nor if the snippit is too long to fit, http://hasteb.in/");
         return builder.build();
     }
 
