44ed5855311ead7507058142f9e42a66e9c581b4
Kemikals <waitinferage21@gmail.com>
2020-05-02 19:29:15 -1000

    updated to require reaction from original author to reformat. added emoji-java dependency.

--- src/main/java/org/learn_java/event/listeners/code_block/CodeBlockListener.java
+++ src/main/java/org/learn_java/event/listeners/code_block/CodeBlockListener.java
@@ -1,5 +1,7 @@
 package org.learn_java.event.listeners.code_block;
 
+import com.vdurmont.emoji.Emoji;
+import com.vdurmont.emoji.EmojiManager;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -18,6 +20,7 @@ public class CodeBlockListener extends ListenerAdapter {
     private static final String THUMBS_UP = "\uD83D\uDC4D";
     private static final String THUMBS_DOWN = "\uD83D\uDC4E";
     private static final String DELETE = "❌";
+    private static final String NUMBERS = EmojiManager.getForAlias("1234").getUnicode();
 
     private final List<CodeBlock> codeBlocks = new ArrayList<>();
 
@@ -28,47 +31,23 @@ public class CodeBlockListener extends ListenerAdapter {
             return;
         }
 
-        String author = event.getAuthor().getName() + "\n";
-        String originalMessage = author + event.getMessage().getContentRaw();
-        String beginning = StringUtils.substringBefore(originalMessage, "```");
-        String code = StringUtils.substringBetween(originalMessage, "```", "```");
-        String end = StringUtils.substringAfterLast(originalMessage, "```");
-        List<String> blockContents = Arrays.stream(code.split("\n")).collect(Collectors.toList());
-        int start = 1;
-        for (int i = 1; i < blockContents.size(); i++) {
-            blockContents.set(i, String.format("%2d %s", start++, blockContents.get(i)));
-        }
-        String language = blockContents.remove(0);
-        StringJoiner joiner = new StringJoiner("\n");
-        blockContents.forEach(joiner::add);
-        MessageBuilder messageBuilder = new MessageBuilder();
-        messageBuilder.append(beginning);
-        messageBuilder.appendCodeBlock(joiner.toString(), language);
-        messageBuilder.append(end);
-
-        if (messageBuilder.getStringBuilder().toString().length() >= 2000) {
-            event.getChannel().sendMessage("Sorry too long, can't add line numbers").queue();
-            return;
-        }
-
-        Message reformatted = messageBuilder.build();
-        event.getMessage().delete().queue();
-
-        MessageAction newMessage = event.getChannel().sendMessage(reformatted);
-        newMessage.queue(sentMessage -> {
-            CodeBlock codeBlock = new CodeBlock(message, sentMessage, message.getAuthor());
-            codeBlocks.add(codeBlock);
-            System.out.println(codeBlocks);
-            sentMessage.addReaction(THUMBS_UP).queue(then -> sentMessage.addReaction(THUMBS_DOWN).queue(last -> sentMessage.addReaction(DELETE).queue()));
-            sentMessage.clearReactions().queueAfter(5, TimeUnit.MINUTES, (delete) -> {
-                System.out.println(codeBlocks.remove(codeBlock));
-            });
-        });
+        message.addReaction(NUMBERS).queue();
     }
 
-    public void onGuildMessageReactionAdd (GuildMessageReactionAddEvent event) {
+    public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
         if (event.getUser().isBot()) {
             return;
+        }
+
+        String messageId = event.getReaction().getMessageId();
+        String emote = event.getReactionEmote().getEmoji();
+        Message messageToFormat = event.getReaction().getChannel().retrieveMessageById(messageId).complete();
+
+
+        if (NUMBERS.equals(emote)) {
+            if (event.getUser().equals(messageToFormat.getAuthor())) {
+                handleFormattingCodeBlock(messageToFormat);
+            }
         }
 
         for (CodeBlock block : codeBlocks) {
@@ -103,6 +82,43 @@ public class CodeBlockListener extends ListenerAdapter {
                 event.getReaction().removeReaction(event.getUser()).queue();
             }
         }
+    }
+
+
+    private void handleFormattingCodeBlock(Message message) {
+        String author = message.getAuthor().getAsMention() + "\n";
+        String originalMessage = "Author: " + author + message.getContentRaw();
+        String beginning = StringUtils.substringBefore(originalMessage, "```");
+        String code = StringUtils.substringBetween(originalMessage, "```", "```");
+        String end = StringUtils.substringAfterLast(originalMessage, "```");
+        List<String> blockContents = Arrays.stream(code.split("\n")).collect(Collectors.toList());
+        int start = 1;
+        for (int i = 1; i < blockContents.size(); i++) {
+            blockContents.set(i, String.format("%2d %s", start++, blockContents.get(i)));
+        }
+        String language = blockContents.remove(0);
+        StringJoiner joiner = new StringJoiner("\n");
+        blockContents.forEach(joiner::add);
+        MessageBuilder messageBuilder = new MessageBuilder();
+        messageBuilder.append(beginning);
+        messageBuilder.appendCodeBlock(joiner.toString(), language);
+        messageBuilder.append(end);
+
+        if (messageBuilder.getStringBuilder().toString().length() >= 2000) {
+            message.getChannel().sendMessage("Sorry too long, can't add line numbers").queue();
+            return;
+        }
+
+        Message reformatted = messageBuilder.build();
+        message.delete().queue();
+
+        MessageAction newMessage = message.getChannel().sendMessage(reformatted);
+        newMessage.queue(sentMessage -> {
+            CodeBlock codeBlock = new CodeBlock(message, sentMessage, message.getAuthor());
+            codeBlocks.add(codeBlock);
+            sentMessage.addReaction(THUMBS_UP).queue(then -> sentMessage.addReaction(THUMBS_DOWN).queue(last -> sentMessage.addReaction(DELETE).queue()));
+            sentMessage.clearReactions().queueAfter(5, TimeUnit.MINUTES, (delete) -> codeBlocks.remove(codeBlock));
+        });
     }
 
     private boolean hasCodeBlock(Message message) {
