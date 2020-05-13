192916c00aab56ee3ffcb4cec0c253cf8f953664
Kemikals <waitinferage21@gmail.com>
2020-05-12 23:19:07 -1000

    Finish db setup

--- src/main/java/org/learn_java/Config.java
+++ src/main/java/org/learn_java/Config.java
@@ -1,5 +1,9 @@
 package org.learn_java;
 
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.jooq.util.sqlite.SQLiteDSL;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,6 +13,7 @@ public class Config {
     private String discordKey;
     private String prefix;
     private String owner;
+    private static DSLContext dslContext;
 
     public Config() {
 
@@ -18,6 +23,7 @@ public class Config {
             discordKey = properties.getProperty("discord.key");
             prefix = properties.getProperty("bot.prefix");
             owner = properties.getProperty("bot.owner.id");
+            dslContext = DSL.using(properties.getProperty("db.url"));
 
         } catch (IOException e) {
             System.out.println("Cannot read settings file");
@@ -35,5 +41,9 @@ public class Config {
 
     public String getOwner() {
         return owner;
+    }
+
+    public static DSLContext getDslContext() {
+        return dslContext;
     }
 }
