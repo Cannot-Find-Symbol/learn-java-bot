1873adab2d1d1558660de3af85f235402920f93d
Kemikals <waitinferage21@gmail.com>
2021-08-20 22:58:00 -1000

    cleanup code

--- src/main/java/org/learn_java/bot/event/listeners/run_listener/RunListener.java
+++ src/main/java/org/learn_java/bot/event/listeners/run_listener/RunListener.java
@@ -2,7 +2,6 @@ package org.learn_java.bot.event.listeners.run_listener;
 
 import com.vdurmont.emoji.EmojiManager;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.ReadyEvent;
@@ -11,38 +10,42 @@ import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEve
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Nonnull;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.function.Consumer;
 
 @Component
 public class RunListener extends ListenerAdapter {
     private static final String RUNNING = EmojiManager.getForAlias("running").getUnicode();
-    private WebClient client = WebClient.create();
-    Map<String, Language> languageMap = new HashMap<>();
+    private static final String EXECUTE = "https://emkc.org/api/v2/piston/execute";
+    private static final String RUNTIMES = "https://emkc.org/api/v2/piston/runtimes";
+    private static final String LANGUAGE_IS_INVALID = "Sorry, can't run this code. No language provided or language is invalid";
+    private final WebClient client;
+    private final Map<String, Language> languageMap;
 
+    public RunListener() {
+        client = WebClient.create();
+        languageMap = new HashMap<>();
+    }
 
     @Override
     public void onReady(@Nonnull ReadyEvent event) {
-        buildLanguageMap();
+        getLanguages();
     }
 
-    private void buildLanguageMap() {
-        client.get().uri("https://emkc.org/api/v2/piston/runtimes")
+    private void getLanguages() {
+        client.get().uri(RUNTIMES)
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<Language>>() {})
+                .bodyToMono(new ParameterizedTypeReference<List<Language>>(){})
                 .subscribe(this::buildLanguageMap);
     }
 
@@ -69,47 +72,55 @@ public class RunListener extends ListenerAdapter {
     }
 
     public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
-        if (event.getUser().isBot()) {
+        if (event.getUser().isBot() || !RUNNING.equals(event.getReactionEmote().getEmoji())) {
             return;
         }
 
         String messageId = event.getReaction().getMessageId();
-        String emote = event.getReactionEmote().getEmoji();
-
-        if(RUNNING.equals(emote)) {
-             event.getReaction().getChannel().retrieveMessageById(messageId).queue(message -> {
-                 String lang = StringUtils.substringBetween(message.getContentRaw(), "```", "\n");
-                 String code = StringUtils.substringBetween(message.getContentRaw(), "```" + lang, "```");
-
-                 if(!languageMap.containsKey(lang)) {
-                    buildLanguageMap();
-                 }
-
-                 if(languageMap.containsKey(lang)) {
-                     Language language = languageMap.get(lang);
-                     try {
-                         client.post()
-                                 .uri(new URI("https://emkc.org/api/v2/piston/execute"))
-                                 .body(Mono.just(new RunRequest(code, language)), RunRequest.class)
-                                 .retrieve()
-                                 .bodyToMono(RunResponse.class)
-                                 .subscribe((r) -> event.getChannel().sendMessageEmbeds(buildResponse(language, r)).queue());
-                     } catch (URISyntaxException ignored) {
-                     }
-                 } else {
-                     event.getChannel().sendMessage("Sorry, can't run this code. No language provided or language is invalid").queue();
-                 }
-             });
-        }
+        event.getReaction().getChannel().retrieveMessageById(messageId)
+                .queue(message -> sendResponseToChannel(event, message));
         event.getReaction().clearReactions().queue();
+    }
+
+    private void sendResponseToChannel(GuildMessageReactionAddEvent event, Message message) {
+        String lang = StringUtils.substringBetween(message.getContentRaw(), "```", "\n");
+        String code = StringUtils.substringBetween(message.getContentRaw(), "```" + lang, "```");
+
+        if (languageMap.containsKey(lang)) {
+            Language language = languageMap.get(lang);
+            sendRequest(event, code, language);
+        } else {
+            event.getChannel().sendMessage(LANGUAGE_IS_INVALID).queue();
+        }
+    }
+
+    private void sendRequest(GuildMessageReactionAddEvent event, String code, Language language) {
+        try {
+            client.post()
+                    .uri(new URI(EXECUTE))
+                    .body(Mono.just(new RunRequest(code, language)), RunRequest.class)
+                    .retrieve()
+                    .bodyToMono(RunResponse.class)
+                    .subscribe(sendResponse(event, language));
+        } catch (URISyntaxException ignored) {
+        }
+    }
+
+    private Consumer<RunResponse> sendResponse(GuildMessageReactionAddEvent event, Language language) {
+        return (r) -> event.getChannel().sendMessageEmbeds(buildResponse(language, r)).queue();
     }
 
     private MessageEmbed buildResponse(Language language, RunResponse r) {
         EmbedBuilder builder = new EmbedBuilder();
         builder.setTitle("Run Code Result");
-        builder.addField("Language", language.getLanguage(), false);
-        builder.addField("Version", language.getVersion(), false);
-        builder.addField("Output", r.getRun().getOutput(), true);
+        builder.addField("Language", language.getLanguage(), true);
+        builder.addField("Version", language.getVersion(), true);
+        builder.addField("Output", r.getRun().getOutput(), false);
         return builder.build();
+    }
+
+    @Scheduled(cron = "0 0 6 * * *")
+    public void refreshLanguageMap() {
+        getLanguages();
     }
 }
