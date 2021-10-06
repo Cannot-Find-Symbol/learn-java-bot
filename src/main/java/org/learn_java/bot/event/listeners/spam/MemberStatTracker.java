c8aa78bcf5eef5513cb66d27b2cd8d85b935204a
Kemikals <Waitinferage21@gmail.com>
2021-10-05 23:05:59 -1000

    repeated message banner thing should be good.

---
+++ src/main/java/org/learn_java/bot/event/listeners/spam/MemberStatTracker.java
@@ -0,0 +1,87 @@
+package org.learn_java.bot.event.listeners.spam;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class MemberStatTracker {
+    private static final int MAX_REPEATED_MESSAGE = 5;
+    private static final int MAX_REPEATED_DIFFERENT_CHANNELS = 2;
+    private static final int UNIQUE_MESSAGES_DUMP_STATS_LIMIT = 5;
+    private final long userId;
+    private final Map<String, Integer> messageFrequencyTracker;
+    private final Map<String, Set<Long>> messageToChannelTracker;
+    private boolean warned;
+    private LocalDateTime lastUpdated;
+    private int uniqueConcurrentMessageCount;
+
+    public MemberStatTracker(long userId) {
+        uniqueConcurrentMessageCount = 0;
+        messageFrequencyTracker = new HashMap<>();
+        messageToChannelTracker = new HashMap<>();
+        this.userId = userId;
+    }
+
+    public void trackMessage(String message, long channelId){
+        if(!messageFrequencyTracker.containsKey(message) && !messageToChannelTracker.containsKey(message)) {
+            uniqueConcurrentMessageCount++;
+        }
+
+        if(uniqueConcurrentMessageCount >= UNIQUE_MESSAGES_DUMP_STATS_LIMIT) {
+            messageFrequencyTracker.clear();
+            messageToChannelTracker.clear();
+            uniqueConcurrentMessageCount = 0;
+        }
+
+        messageFrequencyTracker.put(message, messageFrequencyTracker.getOrDefault(message, 1) + 1);
+        Set<Long> channelIds = messageToChannelTracker.computeIfAbsent(message, (k) -> new HashSet<>());
+        channelIds.add(channelId);
+        lastUpdated = LocalDateTime.now();
+    }
+
+    public boolean memberShouldBeWarned() {
+        return hasTooManyRepeatedMessagesAcrossChannels() || hasTooManyRepeatedMessages();
+    }
+
+    private boolean hasTooManyRepeatedMessages() {
+        return messageFrequencyTracker.values().stream()
+                .anyMatch(value -> value > MAX_REPEATED_MESSAGE);
+    }
+
+    private boolean hasTooManyRepeatedMessagesAcrossChannels() {
+        return messageToChannelTracker.values().stream()
+                .anyMatch(value -> value.size() > MAX_REPEATED_DIFFERENT_CHANNELS);
+    }
+
+    public boolean messageIsInViolation(String message) {
+        Integer messageCount = messageFrequencyTracker.get(message);
+        Set<Long> channelsMessageSentTo = messageToChannelTracker.get(message);
+
+        return (messageCount != null && messageCount > MAX_REPEATED_MESSAGE)
+                || (channelsMessageSentTo != null && channelsMessageSentTo.size() > MAX_REPEATED_DIFFERENT_CHANNELS);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MemberStatTracker that = (MemberStatTracker) o;
+        return userId == that.userId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+
+    public boolean isWarned() {
+        return warned;
+    }
+
+    public void setWarned(boolean warned) {
+        this.warned = warned;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+}
--- src/main/java/org/learn_java/bot/event/listeners/spam/SpamListener.java
+++ src/main/java/org/learn_java/bot/event/listeners/spam/SpamListener.java
@@ -1,12 +1,20 @@
 package org.learn_java.bot.event.listeners.spam;
 
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.exceptions.HierarchyException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.learn_java.bot.data.repositories.SpamRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @ConditionalOnProperty(value = "spam.enabled", havingValue = "true", matchIfMissing = true)
@@ -16,27 +24,58 @@ public class SpamListener extends ListenerAdapter {
     private static final String REASON = "Matched spam filter";
     private final SpamRepository repository;
 
+    private final Map<Long, MemberStatTracker> trackers = new ConcurrentHashMap<>();
+
     public SpamListener(SpamRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        if (event.isWebhookMessage())
-            return;
+        if (event.isWebhookMessage() || event.getMember() == null || event.getAuthor().isBot()) return;
         String message = event.getMessage().getContentRaw().trim();
-        String author = event.getAuthor().getName();
+        Member member = event.getMember();
+        TextChannel channel = event.getChannel();
+        checkForRepeatedMessages(message, member, channel);
+        checkIfMatchesSpam(event, message, event.getAuthor().getName());
+    }
+
+    public void checkForRepeatedMessages(String message, Member member, TextChannel channel) {
+        MemberStatTracker tracker = trackers.computeIfAbsent(member.getUser().getIdLong(), MemberStatTracker::new);
+        tracker.trackMessage(message, channel.getIdLong());
+
+        if (tracker.isWarned() && tracker.messageIsInViolation(message)) {
+            channel.sendMessage("BOOM heashot...").queue();
+            // should have been banned
+            trackers.remove(member.getUser().getIdLong());
+        }
+
+        if (tracker.memberShouldBeWarned() && !tracker.isWarned()) {
+            MessageBuilder builder = new MessageBuilder();
+            builder.setContent("You just hit the rate limit. You've sent too many repeated messages either across channels, or to one channel. Next repeated message will result in a ban ");
+            builder.append(member.getAsMention());
+            channel.sendMessage(builder.build()).queue();
+            tracker.setWarned(true);
+        }
+    }
+
+    private void checkIfMatchesSpam(GuildMessageReceivedEvent event, String message, String author) {
         if (repository.findByMessage(message) != null) {
             Member member = event.getMember();
             if (member != null) {
                 try {
                     member.ban(REMOVE_HISTORY_DAYS, REASON).flatMap(
-                            rest -> event.getChannel().sendMessage(author + " has hit the spam filter, goodbye"))
+                                    rest -> event.getChannel().sendMessage(author + " has hit the spam filter, goodbye"))
                             .queue();
                 } catch (HierarchyException ex) {
                     event.getChannel().sendMessage("You hit the spam filter, but I cannot harm you master").queue();
                 }
             }
         }
+    }
+
+    @Scheduled(cron = "* */15 * * * *")
+    public void purgeOldMessageTrackers() {
+        trackers.values().removeIf(v -> Duration.between(v.getLastUpdated(), LocalDateTime.now()).toMinutes() >= 30);
     }
 }
